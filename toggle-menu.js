7e5fd3fff39329ccbb85e7e4e626f587e817dc2e
Nicole Slaughter <npetecca@asu.edu>
2024-06-07 20:14:54 -0400

    completing desktop responsive design

--- toggle-menu.js
+++ toggle-menu.js
@@ -2,12 +2,12 @@ document
   .getElementById("menu-categories")
   .addEventListener("change", function () {
     "use strict";
-    var vis = document.querySelector(".toggle-on"),
+    var vis = document.querySelector(".toggle-on.menu-wrapper"),
       target = document.getElementById(this.value);
     if (vis !== null) {
-      vis.className = "toggle-off";
+      vis.className = "toggle-off menu-wrapper";
     }
     if (target !== null) {
-      target.className = "toggle-on";
+      target.className = "toggle-on menu-wrapper";
     }
   });
